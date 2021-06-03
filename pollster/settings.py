f446bafc1a1afbe818da3713afd008cb61017f7b
josephine bonka <josephinebonka123@gmail.com>
2021-06-03 15:49:59 +0300

    voting-app2

--- pollster/settings.py
+++ pollster/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
+
+django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
